97d068d139290150fa402ab147e4a037041965fe
jacam666 <ja6cam@gmail.com>
2023-11-17 11:41:19 +0000

    added new resized images and styling

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -4,14 +4,11 @@ function HomePage() {
     return (
         <div>
             <div className="row d-flex justify-content-between">
-                <div className="col-md-4">
-                    <img className='home-page-image img-fluid mt-2 h-100 w-100' src='Images/OIP.jpeg' alt='mezzanine flooring' />
+                <div className="col-md-6">
+                    <img className='home-page-image img-fluid mt-1' src='Images/R_edited.jpg' alt='mezzanine flooring' />
                 </div>
-                <div className="col-md-4">
-                    <img className='home-page-image img-fluid mt-2 h-100' src='Images/Entresol-Noordrek-2017.webp' alt='mezzanine flooring' />
-                </div>
-                <div className="col-md-4">
-                    <img className='home-page-image img-fluid mt-2 h-100' src='Images/838496f8dde33786e17180f7dcb3426d.jpg' alt='mezzanine flooring' />
+                <div className="col-md-6">
+                    <img className='home-page-image img-fluid mt-1' src='Images/838496f8dde33786e17180f7dcb3426d_edited.jpg' alt='mezzanine flooring' />
                 </div>
             </div>
         </div>
