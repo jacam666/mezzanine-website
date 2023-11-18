58e545f6ae6087698550c8a02940c0b88c7f8fbc
jacam666 <ja6cam@gmail.com>
2023-11-18 10:56:13 +0000

    added new contact page details to contact page

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,42 +1,3 @@
-// import React from 'react'
-// import { MDBInput, MDBCheckbox, MDBBtn, MDBTextArea } from 'mdb-react-ui-kit';
-// import '../Styles/ContactUs.css';
-
-
-// function ContactUs() {
-//     return (
-//         <div className='contact-container row m-auto'>
-//             <div>
-//                 <form id='form' className='text-center' style={{ width: '100%', maxWidth: '300px' }}>
-//                     <h2>Contact us</h2>
-
-//                     <MDBInput className='col-lg-6 col-md-6 col-sm-6' label='Name' v-model='name' wrapperClass='mb-4' />
-
-//                     <MDBInput className='col-lg-6 col-md-6 col-sm-6' type='email' label='Email address' v-model='email' wrapperClass='mb-4' />
-
-//                     <MDBInput className='col-lg-6 col-md-6 col-sm-6' label='Subject' v-model='subject' wrapperClass='mb-4' />
-
-//                     <MDBTextArea className='col-lg-6 col-md-6 col-sm-6' wrapperClass='mb-4' label='Message' />
-
-//                     <MDBCheckbox className='col-lg-6 col-md-6 col-sm-6' wrapperClass='d-flex justify-content-center' label='Send me copy' />
-
-//                     <MDBBtn color='primary' block className='my-4'>
-//                         Send
-//                     </MDBBtn>
-//                 </form>
-//                 <iframe className='contact-map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19685.765637188062!2d-0.6835490681688245!3d51.92080659473811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48764e08c08bc8a9%3A0xb6dd641c95115264!2sLeighton%20Buzzard!5e0!3m2!1sen!2suk!4v1700228935945!5m2!1sen!2suk"
-//                     title='Google Maps Location'>
-
-//                 </iframe>
-//             </div>
-//         </div>
-
-
-//     )
-// }
-
-// export default ContactUs
-
 import React, { useState } from 'react';
 import { MDBInput, MDBCheckbox, MDBBtn, MDBTextArea } from 'mdb-react-ui-kit';
 import '../Styles/ContactUs.css';
@@ -60,12 +21,35 @@ function ContactUs() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Add logic to handle form submission, e.g., sending data to a server
         console.log('Form data submitted:', formData);
     };
 
     return (
         <div className='contact-container row m-auto'>
+            <div>
+                <div className='contact-address-container col-lg-2 col-md-6 col-sm-12 mt-2'>
+                    <h1>Address</h1>
+                    <p className='address-info'>
+                        Doherty's Mezzanine Flooring.<br />
+                        16 high street,<br />
+                        Leighton Buzzard,<br />
+                        Lu7 1gg,<br />
+                        England
+                    </p>
+                </div>
+                <div className='contact-phone-container col-lg-2 col-md-6 col-sm-12 mt-2'>
+                    <h1>Phone</h1>
+                    <p className='phone-info'>
+                        01525 999999
+                    </p>
+                </div>
+                <div className='contact-email-container col-lg-2 col-md-6 col-sm-12 mt-2'>
+                    <h1>Email</h1>
+                    <p className='email-info'>
+                        info@dohertysfloors.co.uk
+                    </p>
+                </div>
+            </div>
             <div>
                 <form id='form' className='text-center' onSubmit={handleSubmit}>
                     <h2>Contact us</h2>
