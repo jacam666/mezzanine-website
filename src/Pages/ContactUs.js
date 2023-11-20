d071d86aeeb1b7bae2de5feb0a9a30eca1ce5f91
jacam666 <ja6cam@gmail.com>
2023-11-20 12:39:01 +0000

    removed mdb form component and made my own

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,22 +1,15 @@
 import React, { useState } from 'react';
-import { MDBInput, MDBCheckbox, MDBBtn, MDBTextArea } from 'mdb-react-ui-kit';
 import '../Styles/ContactUs.css';
 
 function ContactUs() {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
-        subject: '',
         message: '',
-        sendCopy: false,
     });
 
     const handleChange = (e) => {
-        const { name, value, type, checked } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: type === 'checkbox' ? checked : value,
-        }));
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
     const handleSubmit = (e) => {
@@ -26,9 +19,9 @@ function ContactUs() {
 
     return (
         <div className='contact-container row m-auto'>
-            <div>
+            <div className='col-lg-12 col-md-6 col-sm-12'>
                 <div className='contact-address-container col-lg-2 col-md-6 col-sm-12 mt-2'>
-                <img src="https://www.freeiconspng.com/uploads/address-icon-25.png" width="100" alt="Hd Icon Address" />
+                    <img src="https://www.freeiconspng.com/uploads/address-icon-25.png" width="80" alt="Hd Icon Address" />
                     <h1>Address</h1>
                     <p className='address-info'>
                         Doherty's Mezzanine Flooring.<br />
@@ -39,80 +32,82 @@ function ContactUs() {
                     </p>
                 </div>
                 <div className='contact-phone-container col-lg-2 col-md-6 col-sm-12 mt-2'>
-                    <img src='/Images/phone-icon-934.png ' width="100" alt="phone icon"/>
+                    <img src='/Images/phone-icon-934.png ' width="80" alt="phone icon" />
                     <h1>Phone</h1>
                     <p className='phone-info'>
                         01525 999999
                     </p>
                 </div>
                 <div className='contact-email-container col-lg-2 col-md-6 col-sm-12 mt-2'>
-                <img src="https://www.freeiconspng.com/uploads/mail-server-icon-png-6.png" width="100" alt="Email Server .ico" />
+                    <img src="https://www.freeiconspng.com/uploads/mail-server-icon-png-6.png" width="80" alt="Email icon" />
                     <h1>Email</h1>
                     <p className='email-info'>
                         info@dohertysfloors.co.uk
                     </p>
                 </div>
             </div>
-            <div>
+            <div className='contact-us-container'>
+                <h2>Contact Us</h2>
                 <form id='form' className='text-center' onSubmit={handleSubmit}>
-                    <h2>Contact us</h2>
-
-                    <MDBInput
-                        className='col-lg-2 col-md-6 col-sm-12'
-                        label='Name'
-                        name='name'
-                        value={formData.name}
-                        onChange={handleChange}
-                        wrapperClass='mb-4'
-                    />
-
-                    <MDBInput
-                        className='col-lg-2 col-md-6 col-sm-12'
-                        type='email'
-                        label='Email address'
-                        name='email'
-                        value={formData.email}
-                        onChange={handleChange}
-                        wrapperClass='mb-4'
-                    />
-
-                    <MDBInput
-                        className='col-lg-2 col-md-6 col-sm-12'
-                        label='Subject'
-                        name='subject'
-                        value={formData.subject}
-                        onChange={handleChange}
-                        wrapperClass='mb-4'
-                    />
-
-                    <MDBTextArea
-                        className='col-lg-2 col-md-6 col-sm-12'
-                        wrapperClass='mb-4'
-                        label='Message'
-                        name='message'
-                        value={formData.message}
-                        onChange={handleChange}
-                    />
-
-                    <MDBCheckbox
-                        className='col-lg-2 col-md-6 col-sm-12'
-                        wrapperClass='d-flex justify-content-center'
-                        label='Send me copy'
-                        name='sendCopy'
-                        checked={formData.sendCopy}
-                        onChange={handleChange}
-                    />
-
-                    <MDBBtn color='primary' block className='my-4 col-lg-2 col-md-6 col-sm-6' type='submit'>
-                        Send
-                    </MDBBtn>
+                    <div className="row m-auto input-bar">
+                        <div className="col-lg-12 col-md-6 col-sm-12">
+                            <label>
+                                Name:
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                    className="form-control"
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="row m-auto input-bar">
+                        <div className="col-lg-12 col-md-6 col-sm-12">
+                            <label>
+                                Email:
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    className="form-control"
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="row m-auto input-bar">
+                        <div className="col-lg-12 col-md-6 col-sm-12">
+                            <label>
+                                Message:
+                                <textarea
+                                    name="message"
+                                    value={formData.message}
+                                    onChange={handleChange}
+                                    className="form-control"
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="row m-auto">
+                        <div className="col-lg-12 col-md-6 col-sm-12">
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
+                            {/* <button onClick={() => handleSubmit(formData)}>Submit</button> */}
+                        </div>
+                    </div>
                 </form>
+            </div>
+            <div className='contact-map-container'>
                 <iframe
                     className='contact-map'
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19685.765637188062!2d-0.6835490681688245!3d51.92080659473811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48764e08c08bc8a9%3A0xb6dd641c95115264!2sLeighton%20Buzzard!5e0!3m2!1sen!2suk!4v1700228935945!5m2!1sen!2suk"
                     title='Google Maps Location'
                 />
             </div>
+
         </div>
     );
 }
