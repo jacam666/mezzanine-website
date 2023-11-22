ce5250aad15f24a63fdb70343b6c697f49a6ca92
jacam666 <ja6cam@gmail.com>
2023-11-22 17:31:00 +0000

    added animation to the text on homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function HomePage() {
+
+    useEffect(() => {
+        // Trigger the animation when the component mounts
+        const textOverlay = document.querySelector('.text-overlay');
+        textOverlay.classList.add('animate');
+    }, []);
+
     return (
         <div className='homepage-container'>
             <div className="row d-flex justify-content-between">
-                <div className="col-lg-6 col-md-6 col-sm-12">
-                    <img className='home-page-image img-fluid mt-1' src='Images/R_edited.jpg' alt='mezzanine flooring' />
+                <div className="col-lg-12 col-md-6 col-sm-12">
+                    <img className='home-page-image img-fluid mt-1' id='homePage-image-1' src='Images/R_edited.jpg' alt='mezzanine flooring' />
+                    <div className='text-overlay'>
+                        We are experts in delivering mezzanine flooring solutions for a diverse range of commercial sectors
+                    </div>
                 </div>
-                <div className="col-lg-6 col-md-6 col-sm-12">
-                    <img className='home-page-image img-fluid mt-1' src='Images/838496f8dde33786e17180f7dcb3426d_edited.jpg' alt='mezzanine flooring' />
-                </div>
+                {/* <div className="col-lg-12 col-md-6 col-sm-12">
+                    <img className='home-page-image img-fluid mt-1' id='homePage-image-2' src='Images/838496f8dde33786e17180f7dcb3426d_edited.jpg' alt='mezzanine flooring' />
+                </div> */}
             </div>
         </div>
 
