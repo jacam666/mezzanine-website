507cf417973070705fee9bcb9b312a17935be9d9
jacam666 <ja6cam@gmail.com>
2023-11-23 14:57:11 +0000

    added animation library to restyle my homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,54 @@
+// import React, { useEffect } from 'react'
+
+// function HomePage() {
+
+//     useEffect(() => {
+//         const textOverlays = document.querySelectorAll('.text-overlay');
+//         textOverlays.forEach((textOverlay) => {
+//             textOverlay.classList.add('animate');
+//         });
+//     }, []);
+
+//     return (
+//         <div className='homepage-container'>
+//             <div className="row d-flex justify-content-between">
+//                 <div className="col-lg-12 col-md-12 col-sm-12">
+//                     <img className='home-page-image img-fluid mt-1' id='homePage-image-1' src='Images/R_edited.jpg' alt='mezzanine flooring' />
+//                     <div className="text-overlay text-overlay-1">
+//                         We are experts in delivering mezzanine flooring solutions for a diverse range of commercial sectors
+//                     </div>
+//                 </div>
+//             </div>
+//             <div className="row d-flex justify-content-between">
+//                 <div className="col-lg-12 col-md-12 col-sm-12">
+//                     <img className='home-page-image img-fluid mt-1' id='homePage-image-2' src='Images/WHITE_FINISH_1.jpeg' alt='mezzanine flooring' />
+//                     <div className="text-overlay text-overlay-2">
+//                         Discover our high-quality finishes that elevate your space
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default HomePage;
+
 import React, { useEffect } from 'react'
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 function HomePage() {
 
     useEffect(() => {
         const textOverlays = document.querySelectorAll('.text-overlay');
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('animate');
+                }
+            });
+        });
         textOverlays.forEach((textOverlay) => {
-            textOverlay.classList.add('animate');
+            observer.observe(textOverlay);
         });
     }, []);
 
@@ -14,19 +57,22 @@ function HomePage() {
             <div className="row d-flex justify-content-between">
                 <div className="col-lg-12 col-md-12 col-sm-12">
                     <img className='home-page-image img-fluid mt-1' id='homePage-image-1' src='Images/R_edited.jpg' alt='mezzanine flooring' />
-                    <div className='text-overlay text-overlay-1'>
+                    <AnimationOnScroll animateIn="animate__fadeInLeftBig" className="text-overlay text-overlay-1">
                         We are experts in delivering mezzanine flooring solutions for a diverse range of commercial sectors
-                    </div>
+                    </AnimationOnScroll>
                 </div>
             </div>
             <div className="row d-flex justify-content-between">
                 <div className="col-lg-12 col-md-12 col-sm-12">
                     <img className='home-page-image img-fluid mt-1' id='homePage-image-2' src='Images/WHITE_FINISH_1.jpeg' alt='mezzanine flooring' />
-                    <div className='text-overlay text-overlay-2'>
+                    <AnimationOnScroll animateIn="animate__fadeInLeftBig" className="text-overlay text-overlay-2">
                         Discover our high-quality finishes that elevate your space
-                    </div>
+                    </AnimationOnScroll>
                 </div>
             </div>
+            {/* <AnimationOnScroll animateIn="animate__bounceIn">
+                        <h2>Some Text</h2>
+                    </AnimationOnScroll> */}
         </div>
     )
 }
