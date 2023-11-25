40ba6b298525720a25f0fe2c0b33ff5f382a579b
jacam666 <ja6cam@gmail.com>
2023-11-25 21:07:22 +0000

    restyled and added animation to the about us page

--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import '../Styles/AboutUs.css'
+import { AnimationOnScroll } from 'react-animation-on-scroll'
 
 function AboutUsPage() {
     return (
         <div className='about-us-container'>
             <h1 className='about-us-header'>About Us:</h1>
             <div className='row'>
-            <p className='about-us-description  col-12' id='paragraph-1'>
+            <AnimationOnScroll animateIn="animate__fadeInLeftBig"  className='about-us-description paragraph-1 col-12 mt-0'>
                 Master Mezzanine is a leading provider of bespoke mezzanine flooring solutions for various sectors and applications. Whether you need to increase your storage space, expand your office area, or create a new retail floor, we can design, manufacture, and install a mezzanine floor that suits your needs and budget.
-            </p>
-            <p className='about-us-description  col-12'>
+            </AnimationOnScroll>
+            <AnimationOnScroll animateIn="animate__fadeInLeftBig"  className='about-us-description  col-12'>
                 We have over 30 years of experience in the mezzanine flooring industry, and we have worked with some of the UK’s biggest brands, such as John Lewis, Big Yellow, and Readie Construction. We use the latest CAD technology to create customised mezzanine floors that maximise the use of your available space and comply with all the relevant building and fire regulations.
-            </p>
-            <p className='about-us-description col-12'>
+            </AnimationOnScroll>
+            <AnimationOnScroll animateIn="animate__fadeInLeftBig"  className='about-us-description col-12'>
                 Our mezzanine floors are made from high-quality materials and components, and we offer a range of accessories and options to enhance the functionality and aesthetics of your mezzanine floor, such as staircases, handrails, pallet gates, decking, and partitioning. We also offer a complete service, from initial site survey and quotation, to technical drawings and approvals, to manufacturing and installation.
-            </p>
-            <p className='about-us-description col-12'>
+            </AnimationOnScroll>
+            <AnimationOnScroll animateIn="animate__fadeInLeftBig"  className='about-us-description col-12'>
                 At Master Mezzanine, we pride ourselves on our customer service and satisfaction. We aim to deliver your mezzanine floor on time and within budget, with minimal disruption to your business operations. We also offer after-sales support and maintenance to ensure your mezzanine floor remains in optimal condition.
-            </p>
-            <p className='about-us-description col-12' id='paragraph-5'>
+            </AnimationOnScroll>
+            <AnimationOnScroll animateIn="animate__fadeInLeftBig"  className='about-us-description col-12' id='paragraph-5'>
                 If you are looking for a reliable and professional mezzanine flooring installation company, look no further than Mezzanine Floors UK. Contact us today to find out how we can help you transform your space with a mezzanine floor.
-            </p>
+            </AnimationOnScroll>
             </div>
             
         </div>
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,38 +1,3 @@
-// import React, { useEffect } from 'react'
-
-// function HomePage() {
-
-//     useEffect(() => {
-//         const textOverlays = document.querySelectorAll('.text-overlay');
-//         textOverlays.forEach((textOverlay) => {
-//             textOverlay.classList.add('animate');
-//         });
-//     }, []);
-
-//     return (
-//         <div className='homepage-container'>
-//             <div className="row d-flex justify-content-between">
-//                 <div className="col-lg-12 col-md-12 col-sm-12">
-//                     <img className='home-page-image img-fluid mt-1' id='homePage-image-1' src='Images/R_edited.jpg' alt='mezzanine flooring' />
-//                     <div className="text-overlay text-overlay-1">
-//                         We are experts in delivering mezzanine flooring solutions for a diverse range of commercial sectors
-//                     </div>
-//                 </div>
-//             </div>
-//             <div className="row d-flex justify-content-between">
-//                 <div className="col-lg-12 col-md-12 col-sm-12">
-//                     <img className='home-page-image img-fluid mt-1' id='homePage-image-2' src='Images/WHITE_FINISH_1.jpeg' alt='mezzanine flooring' />
-//                     <div className="text-overlay text-overlay-2">
-//                         Discover our high-quality finishes that elevate your space
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default HomePage;
-
 import React, { useEffect } from 'react'
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 
